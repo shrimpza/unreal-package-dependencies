7603e674284f24b904edcad65e2f2716ac330352
Kenneth Watson <git@shrimpworks.za.net>
2019-09-24 13:28:12 +0200

    implement simple tests

--- src/test/java/net/shrimpworks/unreal/dependencies/DependencyResolverTest.java
+++ src/test/java/net/shrimpworks/unreal/dependencies/DependencyResolverTest.java
@@ -5,6 +5,8 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.Map;
+import java.util.Set;
 import java.util.zip.GZIPInputStream;
 
 import org.junit.jupiter.api.AfterAll;
@@ -12,16 +14,18 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class DependencyResolverTest {
 
+	private Path tmpDir;
 	private Path unrMap;
 
 	@BeforeAll
 	public void setup() throws IOException {
-		unrMap = Files.createTempFile("test-map-", ".unr");
+		tmpDir = Files.createTempDirectory("deps-test");
+		unrMap = Files.createFile(tmpDir.resolve("SCR-CityStreet.unr"));
 		try (InputStream is = DependencyResolverTest.class.getResourceAsStream("SCR-CityStreet.unr.gz");
 			 GZIPInputStream gis = new GZIPInputStream(is)) {
 			Files.copy(gis, unrMap, StandardCopyOption.REPLACE_EXISTING);
@@ -31,15 +35,20 @@ public class DependencyResolverTest {
 	@AfterAll
 	public void teardown() throws IOException {
 		Files.deleteIfExists(unrMap);
+		Files.deleteIfExists(tmpDir);
 	}
 
 	@Test
-	public void loadPackages() {
-		fail("todo");
+	public void loadPackages() throws IOException {
+		DependencyResolver resolver = new DependencyResolver(unrMap.getParent(), new NativePackages());
+		assertNotNull(resolver.findPackage(unrMap));
+		assertNotNull(resolver.findPackage("SCR-CityStreet"));
 	}
 
 	@Test
-	public void resolveDependencies() {
-		fail("todo");
+	public void resolveDependencies() throws IOException {
+		DependencyResolver resolver = new DependencyResolver(unrMap.getParent(), new NativePackages());
+		Map<String, Set<Resolved>> resolve = resolver.resolve("SCR-CityStreet");
+		assertTrue(resolve.get("SoccerScores").isEmpty());
 	}
 }
