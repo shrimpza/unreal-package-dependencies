b8a2c9543c5e14300a46f17451151fba9a4fdd19
Kenneth Watson <git@shrimpworks.za.net>
2020-05-25 18:18:53 +0200

    move shipped packages to resrouces, upgrade gradle

--- src/test/java/net/shrimpworks/unreal/dependencies/ShippedPackagesTest.java
+++ src/test/java/net/shrimpworks/unreal/dependencies/ShippedPackagesTest.java
@@ -2,12 +2,19 @@ package net.shrimpworks.unreal.dependencies;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class ShippedPackagesTest {
 
 	@Test
 	public void packagesContain() {
-		assert(ShippedPackages.UNREAL_GOLD.contains("DmDeck16"));
-		assert(ShippedPackages.UNREAL_TOURNAMENT.contains("DM-Deck16]["));
-		assert(ShippedPackages.UNREAL_TOURNAMENT_2004.contains("DM-Deck17"));
+		assertTrue(ShippedPackages.UNREAL_GOLD.contains("DmDeck16"));
+		assertTrue(ShippedPackages.UNREAL_TOURNAMENT.contains("DM-Deck16]["));
+		assertTrue(ShippedPackages.UNREAL_TOURNAMENT_2004.contains("DM-Deck17"));
+
+		assertFalse(ShippedPackages.UNREAL_GOLD.contains("DM-Deck17"));
+		assertFalse(ShippedPackages.UNREAL_TOURNAMENT.contains("DmDeck16"));
+		assertFalse(ShippedPackages.UNREAL_TOURNAMENT_2004.contains("DM-Deck16]["));
 	}
 }
